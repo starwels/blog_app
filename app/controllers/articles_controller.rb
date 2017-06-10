79a56ba2d48b65a2dde01462d8b32de6723c8c71
Wellington Santos <wlss.lucas@gmail.com>
2017-06-10 20:14:01 -0300

    add bootstrap and autoprefixer

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   def create
   	@article = Article.new(article_params)
   	if @article.save
-  		flash[:success] = "Article has been created"
+  		flash[:notice] = "Article has been created"
   		redirect_to articles_path
   	end
   end
