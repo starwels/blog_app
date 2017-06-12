d73db91ff05045596af878653d9ac9665d6fdd36
Wellington Santos <wlss.lucas@gmail.com>
2017-06-11 22:53:05 -0300

    Implementing displaying message when no article is present

--- spec/features/create_article_spec.rb
+++ spec/features/create_article_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.feature "Creating Articles" do
+RSpec.feature "Creating Articles - " do
 	scenario "A user creates a new article" do
 		visit "/"
 		click_link "New Article"
